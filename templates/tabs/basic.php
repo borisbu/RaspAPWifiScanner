f16e33065b3e30a424ceda3b146350d84fd622cd
billz <billzimmerman@gmail.com>
2024-11-07 00:46:43 -0800

    Encapsulate handlePageAction() in plugin class

--- templates/tabs/basic.php
+++ templates/tabs/basic.php
@@ -3,7 +3,7 @@
   <div class="row">
     <div class="mb-3 col-12 mt-2">
       <div class="row">
-        <div class="col-6">
+        <div class="col-12">
           <?php echo htmlspecialchars($content); ?>
         </div>
       </div>
